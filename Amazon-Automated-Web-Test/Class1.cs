76472477cf6fd916fe88edcb08145ac70ead3bad
Abdirashiid Jama <rashiid_jacfar@hotmail.com>
2020-05-18 20:09:48 +0100

    Initial Commit

---
+++ Amazon-Automated-Web-Test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon_Automated_Web_Test
+{
+    public class Class1
+    {
+    }
+}
